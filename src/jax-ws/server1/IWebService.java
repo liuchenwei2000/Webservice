3941456a4a251fb962bd18cd0d209539fb3aa8e2
Vic Liu <liuchenwei2000@163.com>
2014-06-13 14:08:01 +0800

    初始化

---
+++ src/jax-ws/server1/IWebService.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package server1;
+
+import javax.jws.WebService;
+
+/**
+ * webservice的接口类
+ * 
+ * @author 刘晨伟
+ * 
+ * 创建日期：2014年5月21日
+ */
+@WebService
+public interface IWebService {
+
+	public String sayHello();
+}
